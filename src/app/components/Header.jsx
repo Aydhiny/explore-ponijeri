db67275388faee2acf8222850fd3f13c65fc136f
Ajdin Mehmedović <ayden.diagrampixel@gmail.com>
2024-12-06 21:54:20 +0100

    navbar and prototype header added

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,22 +1,65 @@
-import React from "react";
-import Ponijeri from "../images/ponijeri.png";
+"use client";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+
+// Slike koje treba da se menjaju
+const images = [
+  new URL("../images/ponijeri.png", import.meta.url),
+  new URL("../images/ponijeri2.png", import.meta.url),
+  new URL("../images/ponijeri3.png", import.meta.url),
+];
 
 export default function Header() {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  // Interval za promenu slika
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => (prev + 1) % images.length);
+    }, 5000); // Promena slike na svakih 5 sekundi
+
+    return () => clearInterval(interval); // Čisti interval kad komponenta nestane
+  }, []);
+
   return (
-    <div
-      className="relative bg-cover bg-center p-24"
-      style={{
-        backgroundImage: `url(${Ponijeri})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="absolute inset-0 flex items-center justify-center text-main-color-dark-green">
-        <div className="text-center">
-          <h1 className="text-9xl font-bold">
-            Explore <span>Ponijeri</span>
+    <div className="w-full h-[100vh] relative overflow-hidden">
+      <div
+        className="absolute inset-0 flex transition-transform duration-[1s] ease-in-out"
+        style={{
+          transform: `translateX(-${currentImage * 100}%)`, // Pomeri slike horizontalno
+        }}
+      >
+        {/* Mape za prikazivanje slika */}
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="w-full h-full flex-shrink-0 bg-cover bg-center"
+            style={{
+              backgroundImage: `url(${image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+        ))}
+      </div>
+
+      {/* Sadržaj u sredini */}
+      <div className="absolute inset-0 items-center justify-center flex flex-col z-10">
+        <div className="text-center items-center flex flex-col">
+          <p className="text-main-color-lighter-green text-2xl">Dobrodošli</p>
+          <h1 className="text-5xl md:text-8xl lg:text-9xl animate-pulse font-semibold text-white">
+            EXPLORE{" "}
+            <span className="text-main-color-lighter-green">PONIJERI</span>
           </h1>
-          <p className="mt-2 text-lg">Your awesome journey begins here!</p>
+          <div className="mt-4 h-1 w-1/3 mx-auto bg-gradient-to-t from-main-color-lighter-green via-green-500 to-main-color-dark-green rounded-full"></div>
+          <div className="flex m-4">
+            <button className="text-main-color-dark-green border-b-2 mr-8 hover:bg-black hover:text-white transition-all duration-150 hover:border-none border-main-color-dark-green rounded-sm font-bold shadow-xl px-8 py-3 bg-main-color-lighter-green">
+              Obilazak
+            </button>
+            <button className="text-white border-2 hover:border-none hover:bg-main-color-lighter-green transition-all duration-150 hover:text-main-color-dark-green border-white rounded-sm font-bold shadow-xl px-8 py-3 bg-none">
+              O nama
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,28 +4,40 @@ import Tree from "../images/tree.png";
 import Image from "next/image";
 export default function Navbar() {
   return (
-    <div className="p-4 bg-gradient-to-r from-main-color-light-green to-main-color-lighter-green border-b border-main-color-dark-green border-opacity-10 bg-opacity-50">
-      <ul className="flex justify-between items-center text-main-color-dark-green">
-        <Image alt="tree" src={Tree} height={26} width={26} />
-        <li className="mr-10">
+    <div className="p-4 sticky top-0 shadow-2xl z-50 bg-gradient-to-l from-[#a7ffc4e5] to-[#e9ffcf85] backdrop-blur-sm border-b border-y-main-color-light-green">
+      <ul className="flex justify-center font-semibold items-center text-main-color-dark-green text-sm">
+        <Image
+          alt="tree"
+          className="mr-10 -rotate-12"
+          src={Tree}
+          height={26}
+          width={26}
+        />
+        <li className="mr-10 transition-all duration-150 hover:text-black">
           <Link href="/">Početna</Link>
         </li>
-        <li className="mr-10">
+        <li className="mr-10 transition-all duration-150 hover:text-black">
           <Link href="/images">Slike</Link>
         </li>
-        <li className="mr-10">
+        <li className="mr-10 transition-all duration-150 hover:text-black">
           <Link href="/misc">Restorani</Link>
         </li>
-        <li className="mr-10">
+        <li className="mr-10 transition-all duration-150 hover:text-black">
           <Link href="/stay">Prebivališta</Link>
         </li>
-        <li className="mr-10">
+        <li className="mr-10 transition-all duration-150 hover:text-black">
           <Link href="/activities">Aktivnosti</Link>
         </li>
-        <li>
+        <li className="transition-all duration-150 hover:text-black">
           <Link href="/about">O nama</Link>
         </li>
-        <Image alt="tree" src={Tree} height={26} width={26} />
+        <Image
+          alt="tree"
+          className="ml-10 rotate-12"
+          src={Tree}
+          height={26}
+          width={26}
+        />
       </ul>
     </div>
   );
